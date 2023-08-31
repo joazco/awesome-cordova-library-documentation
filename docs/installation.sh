b86a39bedbf1dfb15d83c69e4437314fc6704976
Jordan AZOULAY <jazoulay@joazco.com>
2023-08-31 22:46:39 +0200

    move all to plugins folder

--- docs/installation.sh
+++ docs/installation.sh
@@ -1,44 +1,46 @@
-mkdir docs/batterystatus && ln @awesome-cordova-library/batterystatus/README.md docs/batterystatus
+ln @awesome-cordova-library/README.md docs/plugins/
 
-mkdir docs/camera && ln @awesome-cordova-library/camera/README.md docs/camera
+mkdir docs/plugins/batterystatus && ln @awesome-cordova-library/batterystatus/README.md docs/plugins/batterystatus
 
-mkdir docs/device && ln @awesome-cordova-library/device/README.md docs/device
+mkdir docs/plugins/camera && ln @awesome-cordova-library/camera/README.md docs/plugins/camera
 
-mkdir docs/dialogs && ln @awesome-cordova-library/dialogs/README.md docs/dialogs
+mkdir docs/plugins/device && ln @awesome-cordova-library/device/README.md docs/plugins/device
 
-mkdir docs/geolocation && ln @awesome-cordova-library/geolocation/README.md docs/geolocation
+mkdir docs/plugins/dialogs && ln @awesome-cordova-library/dialogs/README.md docs/plugins/dialogs
 
-mkdir docs/globalization && ln @awesome-cordova-library/globalization/README.md docs/globalization
+mkdir docs/plugins/geolocation && ln @awesome-cordova-library/geolocation/README.md docs/plugins/geolocation
 
-mkdir docs/globalization && ln @awesome-cordova-library/globalization/README.md docs/globalization
+mkdir docs/plugins/globalization && ln @awesome-cordova-library/globalization/README.md docs/plugins/globalization
 
-mkdir docs/inappbrowser && ln @awesome-cordova-library/inappbrowser/README.md docs/inappbrowser
+mkdir docs/plugins/globalization && ln @awesome-cordova-library/globalization/README.md docs/plugins/globalization
 
-mkdir docs/localStorage && ln @awesome-cordova-library/localStorage/README.md docs/localStorage
+mkdir docs/plugins/inappbrowser && ln @awesome-cordova-library/inappbrowser/README.md docs/plugins/inappbrowser
 
-mkdir docs/media && ln @awesome-cordova-library/media/README.md docs/media
+mkdir docs/plugins/localStorage && ln @awesome-cordova-library/localStorage/README.md docs/plugins/localStorage
 
-mkdir docs/nativeaudio && ln @awesome-cordova-library/nativeaudio/README.md docs/nativeaudio
+mkdir docs/plugins/media && ln @awesome-cordova-library/media/README.md docs/plugins/media
 
-mkdir docs/network-information && ln @awesome-cordova-library/network-information/README.md docs/network-information
+mkdir docs/plugins/nativeaudio && ln @awesome-cordova-library/nativeaudio/README.md docs/plugins/nativeaudio
 
-mkdir docs/onesignal && ln @awesome-cordova-library/onesignal/README.md docs/onesignal
+mkdir docs/plugins/network-information && ln @awesome-cordova-library/network-information/README.md docs/plugins/network-information
 
-mkdir docs/ratedialog && ln @awesome-cordova-library/ratedialog/README.md docs/ratedialog
+mkdir docs/plugins/onesignal && ln @awesome-cordova-library/onesignal/README.md docs/plugins/onesignal
 
-mkdir docs/screen-orientation && ln @awesome-cordova-library/screen-orientation/README.md docs/screen-orientation
+mkdir docs/plugins/ratedialog && ln @awesome-cordova-library/ratedialog/README.md docs/plugins/ratedialog
 
-mkdir docs/shake && ln @awesome-cordova-library/shake/README.md docs/shake
+mkdir docs/plugins/screen-orientation && ln @awesome-cordova-library/screen-orientation/README.md docs/plugins/screen-orientation
 
-mkdir docs/social-sharing && ln @awesome-cordova-library/social-sharing/README.md docs/social-sharing
+mkdir docs/plugins/shake && ln @awesome-cordova-library/shake/README.md docs/plugins/shake
 
-mkdir docs/splashscreen && ln @awesome-cordova-library/splashscreen/README.md docs/splashscreen
+mkdir docs/plugins/social-sharing && ln @awesome-cordova-library/social-sharing/README.md docs/plugins/social-sharing
 
-mkdir docs/statusbar && ln @awesome-cordova-library/statusbar/README.md docs/statusbar
+mkdir docs/plugins/splashscreen && ln @awesome-cordova-library/splashscreen/README.md docs/plugins/splashscreen
 
-mkdir docs/taptic-engine && ln @awesome-cordova-library/taptic-engine/README.md docs/taptic-engine
+mkdir docs/plugins/statusbar && ln @awesome-cordova-library/statusbar/README.md docs/plugins/statusbar
 
-mkdir docs/vibration && ln @awesome-cordova-library/vibration/README.md docs/vibration
+mkdir docs/plugins/taptic-engine && ln @awesome-cordova-library/taptic-engine/README.md docs/plugins/taptic-engine
+
+mkdir docs/plugins/vibration && ln @awesome-cordova-library/vibration/README.md docs/plugins/vibration
 
 
 
